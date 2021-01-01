283638e83c03a9132fe512db3c3f108f5b9f6231
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-01-01 23:43:36 +0530

    Reverse the Array🚀🚀

---
+++ Arrays/Reverse the Array.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void rvereseArray(int arr[], int start, int end)
+{
+	while (start < end)
+	{
+		int temp = arr[start]; 
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	} 
+}	 
+
+
+void printArray(int arr[], int size)
+{
+for (int i = 0; i < size; i++)
+cout << arr[i] << " ";
+
+cout << endl;
+} 
+
+int main() 
+{
+	int n;
+	cin>>n;
+	
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	
+
+	rvereseArray(arr, 0, n-1);
+	
+ cout<<endl;
+	
+	printArray(arr, n);
+	
+	return 0;
+}
+
