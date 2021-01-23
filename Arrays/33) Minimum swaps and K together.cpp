a889a67e873cbb998df8ed40a939ad9fbb14bd09
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-01-23 22:58:45 +0530

    Minimum swaps and K together🚀🚀

---
+++ Arrays/33) Minimum swaps and K together.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+int minSwap(int *arr, int n, int k);
+
+int main() {
+
+	int t,n,k;
+	cin>>t;
+	while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>k;
+        cout << minSwap(arr, n, k) << "\n";
+    }
+	return 0;
+}
+
+
+
+int minSwap(int *arr, int n, int k) { 
+
+    int count = 0; 
+    for (int i = 0; i < n; ++i) 
+        if (arr[i] <= k) 
+            ++count; 
+      
+    int bad = 0; 
+    for (int i = 0; i < count; ++i) 
+        if (arr[i] > k) 
+            ++bad; 
+      
+
+    int ans = bad; 
+    for (int i = 0, j = count; j < n; ++i, ++j) { 
+          
+        if (arr[i] > k) 
+            --bad; 
+
+        if (arr[j] > k) 
+            ++bad; 
+          
+ 
+        ans = min(ans, bad); 
+    } 
+    return ans; 
+} 
+  
