95719efc37a45a6229be998a1d7decf2aebfe5da
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-01-31 23:14:03 +0530

    Vowel or Not🚀🚀

---
+++ Vowel or Not.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+class Solution{   
+public:
+    string isVowel(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U' )
+        cout<<"YES";
+        else
+        cout<<"NO";
+    }
+};
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        char c;
+        cin >> c;
+        Solution ob;
+        cout << ob.isVowel(c) << endl;
+    }
+    return 0; 
+}   
