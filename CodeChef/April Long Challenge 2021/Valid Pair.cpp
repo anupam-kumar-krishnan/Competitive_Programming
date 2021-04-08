b9c9d947896459fbf4304cdf783da67a79039548
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-08 22:57:03 +0530

    Valid Pair🚀🚀

---
+++ CodeChef/April Long Challenge 2021/Valid Pair.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    int count=0;
+    if(a==b)
+    cout<<"YES"<<endl;
+    else if(a==c)
+    cout<<"YES"<<endl;
+    else if(b==c)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+	// your code goes here
+	return 0;
+}
