ab1d579c369768d897e2ef76a0b8f389c313fee7
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-10 10:28:57 +0530

    Queue at the School🚀🚀

---
+++ Codeforces/Queue at the School.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+    while (t--)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            if (s[i] == 'G' && s[i-1] == 'B')
+            {
+                s[i] = 'B';
+                s[i-1] = 'G';
+                ++i;
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
