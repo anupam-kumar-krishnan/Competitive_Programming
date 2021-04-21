f06b35f5fd3c454f38fa7b9e6faadbfa2cc97c61
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-22 00:29:33 +0530

    Chef and An Ideal Problem🚀🚀

---
+++ CodeChef/Practice/Chef and An Ideal Problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<1<<endl;
+	int newnum;
+	    cin>>newnum;
+	
+	if(newnum==2)
+	    cout<<3;
+	else
+	    cout<<2;
+	
+	return 0;
+}
