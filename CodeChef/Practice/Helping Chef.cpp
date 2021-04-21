804d8b972a4abee815f452364190c7080818ad1a
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-22 00:30:53 +0530

    Helping Chef🚀🚀

---
+++ CodeChef/Practice/Helping Chef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--)
+    {   int t;
+        cin>>t;
+        if(t<10)
+        cout<<"Thanks for helping Chef!"<<endl;
+        else
+        cout<<"-1"<<endl;
+    }
+    return 0;
+}
