2e149e0b58c090daadfbb3b17ba55518fa5185fd
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-21 09:39:43 +0530

    Finding Square Roots🚀🚀

---
+++ CodeChef/Practice/Finding Square Roots.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int result;
+	    result= sqrt(n);
+	    cout<<result<<endl;
+	}
+}
