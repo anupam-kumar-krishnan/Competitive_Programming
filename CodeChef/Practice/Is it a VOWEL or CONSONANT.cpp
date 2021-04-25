9485c700dc0d5ee3b74da4da2d043a1a83426789
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-04-25 14:38:58 +0530

    Is it a VOWEL or CONSONANT🚀🚀

---
+++ CodeChef/Practice/Is it a VOWEL or CONSONANT.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char vow;
+    cin>>vow;
+    if(vow==65||vow==69||vow==73||vow==79||vow==85)
+    cout<<"Vowel";
+    else
+    cout<<"Consonant";
+	// your code goes here
+	return 0;
+}
