5fb8f02c41664a0382a8f10c2d792d2c93294b74
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-06-30 23:49:22 +0530

    Coins And Triangle🚀🚀

---
+++ CodeChef/Practice/Coins And Triangle.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int t;
+	cin>>t;
+	for(int T=0;T<t;T++)
+	{
+		long long int c;
+		cin>>c;
+		long long int s;
+		
+		int ans1=(-1+sqrt(1+4*2*c))/2;
+		 	cout<<ans1<<endl;
+	}
+	return 0;
+}
