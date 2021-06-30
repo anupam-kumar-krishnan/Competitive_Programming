411a0d13bc25de5b5f347bc5f66266ec87ecce51
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-06-30 14:14:53 +0530

    Reverse the array🚀🚀

---
+++ DSA 450/Arrary/1. Reverse the array.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rvereseArray(int arr[], int start, int end)
+{
+	while (start < end)
+	{
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+}	
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+}
+
+
+int main()
+{
+	
+	int n;
+	cin>>n;
+	int arr[n];
+	
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+
+  printArray(arr, n);
+	rvereseArray(arr, 0, n-1);
+	printArray(arr, n);
+	
+	return 0;
+}
