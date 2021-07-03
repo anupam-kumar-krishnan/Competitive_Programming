75af6c5732f35cb8b8ffc9b1470894094c9ca3bc
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-03 20:29:16 +0530

    Program to Swap Two Numbers🚀

---
+++ Basic Programming/Program to Swap Two Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"Enter two numbers: ";
+   cin>>a>>b;
+   
+   cout<<"Before Swapping: "<<endl;
+   cout<<a<<" "<<b<<endl;
+   
+   int temp;
+   temp=a;
+   a=b;
+   b=temp;
+   
+   cout<<"After Swapping: "<<endl;
+   cout<<a<<" "<<b;
+}
