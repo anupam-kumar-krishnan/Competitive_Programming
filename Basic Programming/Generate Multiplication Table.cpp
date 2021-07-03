bcaa5cfcdca8d09d7caa88e0ff82a1ea060e13eb
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-03 20:47:36 +0530

    Generate Multiplication Table✅

---
+++ Basic Programming/Generate Multiplication Table.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   cout<<"Table of "<<n<<":"<<endl;
+   cout<<"***********"<<endl;
+   for(int i=1;i<=10;i++)
+   {
+      cout<<n<<" X "<<i<<"="<<n*i<<endl;
+   }
+
+}
