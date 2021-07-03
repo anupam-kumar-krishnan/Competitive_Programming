7779e725428a0d18c29c9ff1dff41d82f4eed3f2
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-03 20:42:39 +0530

    Sum of Natural Numbers✅

---
+++ Basic Programming/Sum of Natural Numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   int sum=0;
+   for(int i=1;i<=n;++i)
+   {
+       sum+=i;
+   }
+   cout<<"Sum of n Natural Numbers: "<<sum;
+}
