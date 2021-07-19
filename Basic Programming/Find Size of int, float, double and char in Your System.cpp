e4bc60f3bb976d94664257bfd59fccdb5c14fdc2
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-19 19:14:01 +0530

    ✅

---
+++ Basic Programming/Find Size of int, float, double and char in Your System.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+ cout<<"Char: "<<sizeof(char)<<endl;
+ cout<<"Integer: "<<sizeof(int)<<endl;
+ cout<<"Float: "<<sizeof(float)<<endl;
+ cout<<"Long: "<<sizeof(long)<<endl;
+ cout<<"Double: "<<sizeof(double)<<endl;
+
+}
