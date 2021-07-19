3c127c0bacd0b73a401bc72c03767e13ce092b12
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-19 19:40:11 +0530

    Create Leap Year or Not.cpp

---
+++ Basic Programming/Leap Year or Not.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+   int year;
+
+   cout<<"Enter Year: ";
+   cin>>year;
+
+   if(year%4==0 || year%100==0 || year%400==0 )
+      cout<<"Leap Year = "<<year;
+   else
+      cout<<year<<" is not a leap year"<<endl;
+  
+    return 0;
+}
