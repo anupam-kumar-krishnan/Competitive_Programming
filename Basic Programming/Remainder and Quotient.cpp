89c70907ba37e5371b28cad67d6b7ca0c731d7ac
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-19 19:10:45 +0530

    ✅

---
+++ Basic Programming/Remainder and Quotient.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int num1,num2;
+ cout<<"Input two numbers: "<<endl;
+ cin>>num1>>num2;
+ cout<<"Quotient: "<<num1/num2<<endl;
+ cout<<"Remainder: "<<num1%num2<<endl;
+}
