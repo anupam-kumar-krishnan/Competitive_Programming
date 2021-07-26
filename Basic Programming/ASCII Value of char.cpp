f921cf151a9f2372c273b70788f37f74f2466801
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-07-26 10:48:11 +0530

    ASCII Value of char🚀

---
+++ Basic Programming/ASCII Value of char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+  
+    char x;
+    cout<<"Enter the character: ";
+    cin>>x;
+    
+    cout<<"ASCII value of "<<x<<" character: "<<int(x);
+ 
+return 0;
+
+}
