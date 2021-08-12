5cd1e8f70f308de9fae2fbe7c36812e9b258700e
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-12 22:54:51 +0530

    Delete 6. Union of two arrays.cpp

--- DSA 450/6. Union of two arrays.cpp
+++
@@ -1,9 +0,0 @@
-    	    int doUnion(int a[],int n; int b[], int m){
-            set<int> s;
-            for(int i=0;i<n;i++)
-              s.insert(a[i]);
-            for(int i=0;i<n;i++)
-              s.insert(b[i]);
-            return s.size() ;
-          }
-            
