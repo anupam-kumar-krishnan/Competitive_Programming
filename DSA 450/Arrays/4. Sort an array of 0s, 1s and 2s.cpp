70967f7e2e595324a321615d2f5804e40a27d39c
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-15 16:39:41 +0530

    Update 4. Sort an array of 0s, 1s and 2s.cpp

--- DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
+++ DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
@@ -1,14 +1,57 @@
- int maxSubarraySum(int a[], int n){
-        
-        long long maxh = 0, maxf = INT_MIN;
-        
-        for(int i=0;i<n;i++) 
-            {
-                maxh+=a[i];               
-                maxf=max(maxh,maxf);                 
-                if(maxh<0)
-                    maxh=0;
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+class Solution {
+public:
+    void sort012(vector<int>& nums) {
+       int lo = 0; 
+        int hi = nums.size() - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
             }
-     
-            return maxf; 
+        }
     }
+};
+
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n];
+
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+
+		Solution.ob;
+		ob.sort012(a,n);
+
+		for(int i=0;i<n;i++){
+			cout<<a[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
