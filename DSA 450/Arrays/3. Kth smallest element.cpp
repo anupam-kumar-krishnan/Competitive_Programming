c82368a333370a329861daa041467d3cea670ba7
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-17 20:35:42 +0530

    Update 3. Kth smallest element.cpp

--- DSA 450/Arrays/3. Kth smallest element.cpp
+++ DSA 450/Arrays/3. Kth smallest element.cpp
@@ -32,3 +32,43 @@ int main()
 	}
 	return 0;
 }
+------------------------------------------------------------------------------------
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int arr[n];
+        
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int k;
+        cin>>k;
+        priority_queue<int,vector<int>,greater<int>> p;
+        
+        for(int i=0;i<n;i++)
+        {
+            p.push(arr[i]);
+        }
+        int ans,i=1;
+        while(!p.empty())
+        {
+            if(i==k)
+            {
+                ans=p.top();
+                break;
+            }
+            i++;
+            p.pop();
+        }
+        cout<< ans<< "\n";
+    }
+}
