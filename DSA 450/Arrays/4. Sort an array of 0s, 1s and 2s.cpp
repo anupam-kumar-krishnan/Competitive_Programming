1f2d7039da96370d0ffca90947ba7ae1cc4e8785
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-17 20:53:49 +0530

    Update 4. Sort an array of 0s, 1s and 2s.cpp

--- DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
+++ DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
@@ -1,57 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-  
 class Solution {
-public:
-    void sort012(vector<int>& nums) {
-       int lo = 0; 
-        int hi = nums.size() - 1; 
-        int mid = 0; 
-
-        while (mid <= hi) { 
-            switch (nums[mid]) { 
-
-            // If the element is 0 
-            case 0: 
-                swap(nums[lo++], nums[mid++]); 
-                break; 
-
-            // If the element is 1 . 
-            case 1: 
-                mid++; 
-                break; 
-
-            // If the element is 2 
-            case 2: 
-                swap(nums[mid], nums[hi--]); 
-                break; 
+    public:
+        void sort012(int nums[],int n){
+            int lo = 0;
+            int hi = n-1;
+            int mid = 0;
+            
+            while(mid<=hi){
+                switch(nums[mid]){
+                    case 0:
+                        swap(nums[lo++],nums[mid++]);
+                        break;
+                    case 1:
+                        mid++;
+                        break;
+                    case 2:
+                        swap(nums[mid],nums[hi--]);
+                        break;
+                    
+                }
             }
         }
-    }
 };
 
 
-int main(){
-	int t;
-	cin>>t;
 
-	while(t--){
-		int n;
-		cin>>n;
-		int a[n];
 
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
 
-		Solution.ob;
-		ob.sort012(a,n);
-
-		for(int i=0;i<n;i++){
-			cout<<a[i]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+            cin>>n;
+        
+        int arr[n];
+        
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }   
+        Solution ob;
+        ob.sort012(arr,n);
+        
+        for(int i=0;i<n;i++)
+            cout<<arr[i]<<" ";
+            
+        cout<<endl;
+    }
 }
