9bd25e8ad5a85e00e441b78fc668af45c221790b
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-17 11:12:14 +0530

    Update 7. Cyclically rotate an array by one.cpp

--- DSA 450/Arrays/7. Cyclically rotate an array by one.cpp
+++ DSA 450/Arrays/7. Cyclically rotate an array by one.cpp
@@ -1,46 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-  
-#include<bits/stc++.h>
-using namespace std;
-
-class Solution {
-public:
-    int doUnion(int a[],int n; int b[], int m){
-     set<int> s;
-     
-	for(int i=0;i<n;i++)
-       		s.insert(a[i]);
-
-     	for(int i=0;i<n;i++)
-        	s.insert(b[i]);
-
-   return s.size() ;
-	}	
-};
-
-
-int main(){
+int main()
+{
 	int t;
-	cin>>t;
+	  cin>>t;
 
 	while(t--){
-		int n;
-		cin>>n;
-		int a[n],i;
+	int n;
+	cin>>n;
+	int a[n], i;
 
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-		rotate(a,n);
+	for(i=0;i<n;i++)
+	   cin>>a[i];
 
-		for(int i=0;i<n;i++){
-			cout<<a[i];
-		cout<<endl;
-		}
+	rotate(a,n);
 
-		
+	for(i=0;i<n;i++)
+		cout<<a[i];
+	cout<<endl;
+      }	
+   return 0;
+}
+
+void rotate(int arr[], int n){
+	int temp = a[n-1];
+	for(int i=n-1;i>0;i--){
+	    a[i]=a[n-1];
 	}
-	return 0;
+   a[0]=temp;
 }
