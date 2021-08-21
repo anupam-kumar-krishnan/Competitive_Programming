ddd4f092413fd9d59718de69706af4b3c361ad92
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-21 22:59:06 +0530

    Minimize the Heights II🎉

---
+++ DSA 450/Arrays/9.Minimize the Heights II.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        vector<pair<int, int>> v;
+        vector<int> taken(n);
+
+        // we will store all possible heights in a vector
+        for (int i = 0; i < n; i++) {
+            if (arr[i] - k >= 0) {
+                v.push_back({arr[i] - k, i});
+            }
+            v.push_back({arr[i] + k, i});
+        }
+        sort(v.begin(), v.end());
+        int elements_in_range = 0;
+        int left = 0;
+        int right = 0;
+
+        // By two pointer we will traverse v and whenever we will get a range
+        // in which all towers are included, we will update the answer.
+        while (elements_in_range < n && right < v.size()) {
+            if (taken[v[right].second] == 0) {
+                elements_in_range++;
+            }
+            taken[v[right].second]++;
+            right++;
+        }
+        int ans = v[right - 1].first - v[left].first;
+        while (right < v.size()) {
+            if (taken[v[left].second] == 1) {
+                elements_in_range--;
+            }
+            taken[v[left].second]--;
+            left++;
+
+            while (elements_in_range < n && right < v.size()) {
+                if (taken[v[right].second] == 0) {
+                    elements_in_range++;
+                }
+                taken[v[right].second]++;
+                right++;
+            }
+
+            if (elements_in_range == n) {
+                ans = min(ans, v[right - 1].first - v[left].first);
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
