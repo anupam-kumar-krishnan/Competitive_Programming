7124cafad598e17139073558f0a78ba226acd88a
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-23 20:37:01 +0530

    Update 5. Move all negative numbers to beginning.cpp

--- DSA 450/Arrays/5. Move all negative numbers to beginning.cpp
+++ DSA 450/Arrays/5. Move all negative numbers to beginning.cpp
@@ -32,3 +32,29 @@ int main()
 	 printArray(arr, n);
 	 return 0;
 }
+-----------------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int a[n],i;
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+        
+    int j=0;
+    
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        {
+            swap(a[i],a[j]);
+            j++;
+        }
+    }
+    
+    for(i=0;i<n;i++)
+        cout<<a[i]<<" ";
+}
