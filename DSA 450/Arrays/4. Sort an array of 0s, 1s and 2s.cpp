5156e01eb9be8edb6b7406a534b2d8dfdcad381d
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-23 20:24:37 +0530

    Update 4. Sort an array of 0s, 1s and 2s.cpp

--- DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
+++ DSA 450/Arrays/4. Sort an array of 0s, 1s and 2s.cpp
@@ -52,3 +52,41 @@ int main()
         cout<<endl;
     }
 }
+----------------------------------------
+                 Method 2
+----------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int a[n],i;
+    for(int i=0;i<n;++i)
+        cin>>a[i];
+        
+    int c0=0,c1=0,c2=0;
+    for(i=0;i<n;++i)
+    {
+        if(a[i]==0)
+            c0++;
+        else if(a[i]==1)
+            c1++;
+        else
+            c2++;
+    }
+    for(i=0;i<c0;++i)
+        a[i]=0;
+    
+    for(i=c0;i<c0+c1;++i)
+        a[i]=1;
+        
+    while(c2--)
+    {
+        a[i++]=2;
+    }
+    
+    for(i=0;i<n;++i)
+        cout<<a[i]<<" ";
+}
