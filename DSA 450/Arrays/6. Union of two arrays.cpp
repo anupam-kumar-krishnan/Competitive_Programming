b33af5f31d40da588e11a3c3ed8cb74af8ab0719
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-24 19:41:52 +0530

    Update 6. Union of two arrays.cpp

--- DSA 450/Arrays/6. Union of two arrays.cpp
+++ DSA 450/Arrays/6. Union of two arrays.cpp
@@ -1,42 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Solution{
+    public:
+        int doUnions(int a[],int n,int b[],int m){
+            set<int> s;
+            for(int i=0;i<n;i++)
+       	    	s.insert(a[i]);
 
-  
-class Solution {
-public:
-    int doUnion(int a[],int n; int b[], int m){
-     set<int> s;
-     
-	for(int i=0;i<n;i++)
-       	    s.insert(a[i]);
-
-        for(int i=0;i<n;i++)
-            s.insert(b[i]);
+            for(int i=0;i<m;i++)
+             	s.insert(b[i]);
 
      return s.size() ;
-	}	
+   }
 };
 
+int main()
+{
+    int n,m;
+    cin>>n>>m;
 
-int main(){
-	int t;
-	cin>>t;
+    int a[n],b[m];
+    for(int i=0;i<n;i++)
+        cin>>a[n];
 
-	while(t--){
-		int n,m;
-		cin>>n>>m;
-		int a[n],b[m];
-
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-
-		for(int i=0;i<m;i++){
-			cin>>b[i];
-		}
-
-		Solution ob;
-          cout<< ob.doUnions(a, n, b, m) <<endl;
-	}
-	return 0;
+    for(int j=0;j<m;j++)
+        cin>>b[j];
+    
+    Solution ob;
+    cout << ob.doUnions(a,n,b,m);
 }
