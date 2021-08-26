a256f9e938b7f730920821da1a04847999a57323
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-08-27 00:11:47 +0530

    Create Binary Tree into its Mirror Tree.cpp

---
+++ Accolite/Binary Tree into its Mirror Tree.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node
+{
+	int data;
+	struct Node* left;
+	struct Node* right;
+};
+
+struct Node* newNode(int data)
+{
+	struct Node* node = (struct Node*)
+						malloc(sizeof(struct Node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	
+	return(node);
+}
+
+
+
+void mirror(struct Node* node)
+{
+	if (node == NULL)
+		return;
+	else
+	{
+		struct Node* temp;
+		
+		mirror(node->left);
+		mirror(node->right);
+	
+		temp	 = node->left;
+		node->left = node->right;
+		node->right = temp;
+	}
+}
+
+
+void inOrder(struct Node* node)
+{
+	if (node == NULL)
+		return;
+	
+	inOrder(node->left);
+	cout << node->data << " ";
+	inOrder(node->right);
+}
+
+
+int main()
+{
+	struct Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	
+	cout << "Inorder traversal of the constructed"
+		<< " tree is" << endl;
+	inOrder(root);
+	
+	mirror(root);
+
+	cout << "\nInorder traversal of the mirror tree"
+		<< " is \n";
+	inOrder(root);
+	
+	return 0;
+}
