e74df44eb94c9950ff94d7ca67448f5b9f43df5f
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-09-02 23:45:30 +0530

    ✨🎊🎉

---
+++ DSA 450/Arrays/13.Kadane's Algorithm.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    long long maxSubarraySum(int a[],int n){
+        int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+    }
+};
+int main()
+{
+   int n;
+   cin>>n;
+
+   int a[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+   }
+
+   Solution ob;
+   cout<<ob.maxSubarraySum(a,n)<<endl;
+
+}
