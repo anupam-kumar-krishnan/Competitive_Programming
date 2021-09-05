f251c966f83e1289843f349867a8257b3dd232c0
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-09-05 23:16:38 +0530

    strings are rotations of each other or not✅

---
+++ DSA 450/String/strings are rotations of each other or not.cpp
@@ -0,0 +1,22 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+bool areRotations(string str1, string str2)
+{
+
+if (str1.length() != str2.length())
+		return false;
+
+string temp = str1 + str1;
+return (temp.find(str2) != string::npos);
+}
+
+int main()
+{
+string str1 = "AACD", str2 = "ACDA";
+if (areRotations(str1, str2))
+	printf("Strings are rotations of each other");
+else
+	printf("Strings are not rotations of each other");
+return 0;
+}
