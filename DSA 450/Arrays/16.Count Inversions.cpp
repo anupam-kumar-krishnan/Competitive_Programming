be498a3aa9feaed493145152049924b6c1af199b
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-09-15 20:47:38 +0530

    Update 16.Count Inversions.cpp

--- DSA 450/Arrays/16.Count Inversions.cpp
+++ DSA 450/Arrays/16.Count Inversions.cpp
@@ -1,72 +1,30 @@
-class Solution{
-  public:
-    long long my_counter = 0;
-    
-    void merge(long long a[], long long p, long long q, long long r){
-        long long l = q-p+1;
-        long long a1[l];
-    
-        long long l2 = r-q;
-        
-        long long a2[l2];
-        
-        for(long long i = 0;i<l;i++){
-            a1[i] = a[i+p];
-        }
+// 2 4 1 3 5
+// 1 4 2 3 5 - 1 iteration
+// 1 2 4 3 5 - 2 iteration
+// 1 2 3 4 5 - 3 iteration
 
-        for(long long i = 0;i<l2;i++){
-            a2[i] = a[q+i+1];
-        }
-        
-        long long left = 0, right = 0, k = p;
-        
-        while(left < l && right < l2)
-        {
-            if(a1[left] <= a2[right]){
-                a[k] = a1[left];
-                left++;
-            }
-            else{
-                a[k] = a2[right];
-                right++;
+#include <bits/stdc++.h>
+using namespace std;
 
-                my_counter += (l-left); 
-            }
-            k++;
-        }
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
 
-        while(left < l){
-            a[k++] = a1[left++];
-        }
+    return inv_count;
+}
 
-        while(right < l2){
-            
-            a[k++] = a2[right++];
-        }
-    }
-
-
-    void mergeSort(long long a[], long long p, long long r)
-    {
-        if(p < r)
-        {
-            long long q = (p+r)/2;
-
-            mergeSort(a, p, q);
-            
-            mergeSort(a, q+1, r);
-            
-            merge(a, p, q, r);
-        }
-    }
-    
-
-    long long int inversionCount(long long arr[],long long N)
-    {
-        mergeSort(arr,0,N-1);
-        long long int res = my_counter;
-        my_counter = 0;
-        return res;
-    }
-
-};
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+      cin>>arr[i];
+    cout << " Number of inversions are "
+         << getInvCount(arr, n);
+    return 0;
+}
