85bdb66bd66aa50083942ac358c8f6f1653403cc
Anup451 <78673834+Anup451@users.noreply.github.com>
2021-10-05 14:41:52 +0530

    Add files via upload

---
+++ search_algorithum_code/linear_search.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+int linearSearch(int arr[],int n,int key){
+    for (int  i = 0; i < n; i++)
+    {
+        if (arr[i]==key)
+        {
+            return i;
+        }
+        
+    }
+    
+    return -1;
+}
+
+
+int main() {
+      int n;
+      cin>>n;
+
+      int arr[n];
+
+      for (int i = 0; i < n; i++)
+      {
+          cin>>arr[i];
+      }
+
+      int key;
+      cin>>key;
+
+
+
+      cout<<linearSearch(arr,n,key);
+
+
+return 0;
+}
