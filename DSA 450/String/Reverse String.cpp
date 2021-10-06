7bddc5619e1bafde36892164bd521a164b5fc3df
vinaybaliyan11 <87595477+vinaybaliyan11@users.noreply.github.com>
2021-10-06 15:33:01 +0530

    Update Reverse String.cpp
    Instead of using an extra variable, swap function is used to swap two elements.

--- DSA 450/String/Reverse String.cpp
+++ DSA 450/String/Reverse String.cpp
@@ -4,10 +4,7 @@ public:
        int left=0;
        int right=s.size()-1;
        while(left<right){
-           char tmp=s[left];
-           s[left]=s[right];
-           s[right]=tmp;
-           
+           swap(s[left],s[right]);
            left++;
            right--;
        } 
