a29e518e40f630ade9531176514ab17b63890c2c
Nehansh10 <nishuj99.com@gmail.com>
2021-10-09 22:35:28 +0530

    Maximum Pairwise Product Code in Java

---
+++ All Languages/Java Codes/Maximum Pairwise Product.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+
+public class MaxPairwiseProduct {
+
+    public static long maxPairwiseProduct(int[] array){
+		int n = array.length;
+		Arrays.sort(array);
+		return ((long)array[n-1]*(long)array[n-2]);
+	}
+	
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(maxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                    InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
