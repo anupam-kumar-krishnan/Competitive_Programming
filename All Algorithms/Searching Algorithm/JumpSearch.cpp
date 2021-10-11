e59be6af121a52a69eebb807dee1659b0f3e31ff
Chandan Kumar Sahu <chandansahuji123@gmail.com>
2021-10-11 11:39:24 +0530

    Added Jump Search Program in C++
    The program is running successfully and giving correct valid output

---
+++ All Algorithms/Searching Algorithm/JumpSearch.cpp
@@ -0,0 +1,59 @@
+// C++ program to implement Jump Search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int jumpSearch(int arr[], int x, int n)
+{
+	// Finding block size to be jumped
+	int step = sqrt(n);
+
+	// Finding the block where element is
+	// present (if it is present)
+	int prev = 0;
+	while (arr[min(step, n)-1] < x)
+	{
+		prev = step;
+		step += sqrt(n);
+		if (prev >= n)
+			return -1;
+	}
+
+	// Doing a linear search for x in block
+	// beginning with prev.
+	while (arr[prev] < x)
+	{
+		prev++;
+
+		// If we reached next block or end of
+		// array, element is not present.
+		if (prev == min(step, n))
+			return -1;
+	}
+	// If element is found
+	if (arr[prev] == x)
+		return prev;
+
+	return -1;
+}
+
+// Driver program to test function
+int main()
+{
+	int n;
+  cin>>n;//array size
+  
+    int *ar = new int[n];
+  for(int i=0;i<n;i++)cin>>ar[i];
+  
+  int x;
+  cin>>x;//Key to be searched
+	
+	// Find the index of 'x' using Jump Search
+	int index = jumpSearch(arr, x, n);
+
+	// Print the index where 'x' is located
+	cout << "\nNumber " << x << " is at index " << index;
+	return 0;
+}
+
