9806177c309bd013e58781172662d6e863fb8107
Ajith <sadanandanajith@gmail.com>
2021-10-12 23:44:32 +0530

    Added new peak element problem

---
+++ All Data Structures/Arrays/peak_element.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int findPeak(int arr[], int n)
+{
+    
+    if (n == 1)
+      return 0;
+    if (arr[0] >= arr[1])
+        return 0;
+    if (arr[n - 1] >= arr[n - 2])
+        return n - 1;
+ 
+    
+    for (int i = 1; i < n - 1; i++) {
+ 
+        
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+            return i;
+    }
+}
+ 
+
+int main()
+{
+    int arr[] = { 1, 3, 20, 4, 1, 0 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Index of a peak point is "
+         << findPeak(arr, n);
+    return 0;
+}
+ 
