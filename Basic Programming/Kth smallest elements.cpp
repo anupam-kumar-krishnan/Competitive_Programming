0b39cebd7b122c15c850f82474a1c232bbea2ede
Sakshi-source <84678584+Sakshi-source@users.noreply.github.com>
2021-10-21 10:27:18 +0530

    Added cop code for kth smallest element

---
+++ Basic Programming/Kth smallest elements.cpp
@@ -0,0 +1,47 @@
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        sort(arr + l, arr + r + 1);
+        
+       // for(int i = l; i < r + 1; i++)
+        //cout<<arr[i];
+        
+        return arr[k - 1];
+    }
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
