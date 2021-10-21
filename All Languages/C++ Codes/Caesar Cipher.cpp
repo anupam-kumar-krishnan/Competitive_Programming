4d86734f55d542af437c0db625dd2366aeb7015d
Sakshi-source <84678584+Sakshi-source@users.noreply.github.com>
2021-10-21 14:06:56 +0530

    Added cpp code for caesar cipher

---
+++ All Languages/C++ Codes/Caesar Cipher.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+string caesarCipher(string s, int k) {
+    int i = 0;
+    while(s[i]!=NULL)
+    {
+       if(s[i] >= 'A' && s[i] <= 'Z')       
+       {
+            s[i] = char(int((s[i] + k - 65) % 26 + 65));
+       }
+       if(s[i] >= 'a' && s[i] <= 'z')
+       {
+            s[i] = char(int((s[i] + k - 97) % 26 + 97));
+       }
+      i++;
+    }
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string s;
+    getline(cin, s);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = caesarCipher(s, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
