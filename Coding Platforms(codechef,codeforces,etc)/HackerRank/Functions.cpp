dd94214753779834c7c08afa3a891485e5ef0d9c
Puja kumari <59861081+pjkri123@users.noreply.github.com>
2021-10-22 15:52:27 +0530

    Create Functions.cpp

---
+++ Coding Platforms(codechef,codeforces,etc)/HackerRank/Functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
