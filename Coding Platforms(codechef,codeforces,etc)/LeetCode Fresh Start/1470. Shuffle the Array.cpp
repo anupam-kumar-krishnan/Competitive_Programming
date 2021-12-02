fdee5815f2495351e043e64ac941cf57580bef6d
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-12-02 23:43:49 +0530

    1470. Shuffle the Array

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode Fresh Start/1470. Shuffle the Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int s = 0;
+        int e = n;
+        
+        vector<int> v;
+        
+        while(s<e){
+            v.push_back(nums[s]);
+            v.push_back(nums[n]);
+            s++; 
+            n++;
+        }
+        return v;
+    }
+};
