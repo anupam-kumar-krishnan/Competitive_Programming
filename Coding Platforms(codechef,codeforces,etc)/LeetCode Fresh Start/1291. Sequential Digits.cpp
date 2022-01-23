f389ef3f68d5e338f1705357b84d8e17b999b728
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2022-01-23 23:20:09 +0530

    Sequential Digits✅

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode Fresh Start/1291. Sequential Digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int l, int h) {
+        queue<int> q;
+        for(int i = 1; i <= 9; i++) {
+            q.push(i);
+        }
+        vector<int> ret;
+        while(!q.empty()) {
+            int f = q.front();
+            q.pop();
+            if(f <= h && f >= l) {
+                ret.push_back(f);
+            }
+            if(f > h)
+                break;
+            int num = f % 10;
+            if(num < 9) {
+                q.push(f * 10 + (num + 1));
+            }
+        }
+        return ret;
+    }
+};
