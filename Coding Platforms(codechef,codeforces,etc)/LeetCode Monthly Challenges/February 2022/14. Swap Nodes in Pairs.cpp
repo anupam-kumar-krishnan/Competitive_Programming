101ffb3e8577e4c66e451013dfe86dac11bab84c
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2022-02-16 20:53:00 +0530

    Swap Nodes in Pairs✅

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode Monthly Challenges/February 2022/14. Swap Nodes in Pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+         if(!head || !head->next) return head; 
+		
+        ListNode* dummyNode = new ListNode();
+        
+        ListNode* prevNode=dummyNode;
+        ListNode* currNode=head;
+        
+        while(currNode && currNode->next){
+            prevNode->next = currNode->next;
+            currNode->next = prevNode->next->next;
+            prevNode->next->next = currNode;
+            
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        
+        return dummyNode->next;
+    }
+};
