2ca9d71d58995c2d272246af10789effd4c743b9
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2022-03-08 22:30:16 +0530

    Linked List Cycle✅

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode Monthly Challenges/March 2022/8. Linked List Cycle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+	
+	
+        if(head == NULL)
+            return false;
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+               
+
+            if(fast == slow)
+                return true;
+         
+        }
+        
+        return false;
+    }
+};
