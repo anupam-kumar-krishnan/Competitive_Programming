f9fb7f600d57c4d2970d687fd6385abc9285267f
Dil <69421619+de1401@users.noreply.github.com>
2022-10-05 14:59:34 +0530

    add Reverse linked list

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode Fresh Start/206. Reverse-linked-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr = head, *prev = NULL;
+
+        while(curr != NULL) {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+};
