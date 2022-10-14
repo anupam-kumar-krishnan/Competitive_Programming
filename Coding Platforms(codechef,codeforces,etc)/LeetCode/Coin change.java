e2bd60e6e0614161706ab284bc916ec58b92f689
Ankita Dolai <78500268+Ankita1827@users.noreply.github.com>
2022-10-15 01:45:06 +0530

    Create Coin change.java

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode/Coin change.java
@@ -0,0 +1,17 @@
+class Solution {
+   public int coinChange(int[] coins, int amount) {
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < coins.length; i++) {
+            
+            for (int j = coins[i]; j < dp.length; j++) {
+                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
+				
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[dp.length - 1] == Integer.MAX_VALUE ? -1 : dp[dp.length - 1];
+    }
+}
