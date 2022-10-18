c70ef3b050cc7dac4612dc0e7f1138a612b54299
Gaurav Saha <72292857+gaurav-312@users.noreply.github.com>
2022-10-18 23:03:47 +0530

    code for swap nodes in pair
    Example 1:
    
    Input: head = [1,2,3,4]
    Output: [2,1,4,3]
    
    Example 2:
    
    Input: head = []
    Output: []
    
    Example 3:
    
    Input: head = [1]
    Output: [1]

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode/Leetcode_24_Swap_Nodes_in_Pair.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==nullptr || head->next==NULL)   return head;
+        ListNode *temp=head;
+        head=head->next;
+        temp->next=head->next;
+        head->next=temp;
+        temp->next=swapPairs(head->next->next);
+        return head;
+    }
+};
