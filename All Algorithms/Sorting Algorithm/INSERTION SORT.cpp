1ce0945d1bf431356fdbe067d2cfefa7b061e743
CodeK00 <116506951+CodeK00@users.noreply.github.com>
2022-10-23 21:53:39 +0530

    C++ Insertion Sort

--- All Algorithms/Sorting Algorithm/INSERTION SORT.cpp
+++ All Algorithms/Sorting Algorithm/INSERTION SORT.cpp
@@ -1,43 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void insertionSort(int arr[], int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
-		key = arr[i];
-		j = i - 1;
-
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
-}
-
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-int main()
-{
-	int arr[] = { 95,15,5,85,65,55};
-	int N = sizeof(arr) / sizeof(arr[0]);
-    cout<<"\nBEFORE SORTING..........\n";
-    printArray(arr, N);
-    cout<<"\n";
-	insertionSort(arr, N);
-    cout<<"\nAFTER SORTING..........\n";
-	printArray(arr, N);
-    cout<<"\n";
-
-	return 0;
-}
-
+void duplicateZeros(vector<int>& arr) 
+    {
+        int n = arr.size();
+        for (int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                for(int j=n-2;j>=i;j--)
+                {
+                    arr[j+1]=arr[j];
+                }
+                i++;
+            }
+        }
+        
+    }
