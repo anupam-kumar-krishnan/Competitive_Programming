c0f31988a1ba950f0f3dcf42061cea2effc514b4
Biswajit Sahoo <biswatian123@gmail.com>
2022-10-23 21:49:16 +0530

    solved hackkerrank linkedin questions

---
+++ Coding Platforms(codechef,codeforces,etc)/HackerRank/insert_node_at_tail.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode
+{
+public:
+    int data;
+    SinglyLinkedListNode *next;
+
+    SinglyLinkedListNode(int node_data)
+    {
+        this->data = node_data;
+        this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList
+{
+public:
+    SinglyLinkedListNode *head;
+
+    SinglyLinkedList()
+    {
+        this->head = nullptr;
+    }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode *node, string sep, ofstream &fout)
+{
+    while (node)
+    {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node)
+        {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode *node)
+{
+    while (node)
+    {
+        SinglyLinkedListNode *temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+SinglyLinkedListNode *insertNodeAtTail(SinglyLinkedListNode *head, int data)
+{
+
+    SinglyLinkedListNode *node_to_be_inserted = new SinglyLinkedListNode(data);
+    if (!head)
+    {
+        return node_to_be_inserted;
+    }
+    SinglyLinkedListNode *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = node_to_be_inserted;
+    return head;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList *llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++)
+    {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        SinglyLinkedListNode *llist_head = insertNodeAtTail(llist->head, llist_item);
+        llist->head = llist_head;
+    }
+
+    print_singly_linked_list(llist->head, "\n", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist->head);
+
+    fout.close();
+
+    return 0;
+}
