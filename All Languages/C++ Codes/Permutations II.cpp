e41718abfc1cfb9d35666de231bc17d986027402
itsrahulprince <91690860+itsrahulprince@users.noreply.github.com>
2022-10-25 19:45:28 +0530

    Create Permutations II.cpp

---
+++ All Languages/C++ Codes/Permutations II.cpp
@@ -0,0 +1,18 @@
+   
+        void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
