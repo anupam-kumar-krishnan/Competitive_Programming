205b23be99c3d3b6286d4770bbee4f149b40b412
tush47 <110597823+tush47@users.noreply.github.com>
2022-10-26 22:28:39 +0530

    Create 719. Find K-th Smallest Pair Distance.cpp

---
+++ Coding Platforms(codechef,codeforces,etc)/LeetCode/719. Find K-th Smallest Pair Distance.cpp
@@ -0,0 +1,59 @@
+// The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+// Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,1], k = 1
+// Output: 0
+// Explanation: Here are all the pairs:
+// (1,3) -> 2
+// (1,1) -> 0
+// (3,1) -> 2
+// Then the 1st smallest distance pair is (1,1), and its distance is 0.
+// Example 2:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 0
+
+
+// answer:
+
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int WIDTH = 2 * nums[nums.length - 1];
+
+        //multiplicity[i] = number of nums[j] == nums[i] (j < i)
+        int[] multiplicity = new int[nums.length];
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[i-1]) {
+                multiplicity[i] = 1 + multiplicity[i - 1];
+            }
+        }
+
+        //prefix[v] = number of values <= v
+        int[] prefix = new int[WIDTH];
+        int left = 0;
+        for (int i = 0; i < WIDTH; ++i) {
+            while (left < nums.length && nums[left] == i) left++;
+            prefix[i] = left;
+        }
+
+        int lo = 0;
+        int hi = nums[nums.length - 1] - nums[0];
+        while (lo < hi) {
+            int mi = (lo + hi) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                count += prefix[nums[i] + mi] - prefix[nums[i]] + multiplicity[i];
+            }
+            //count = number of pairs with distance <= mi
+            if (count >= k) hi = mi;
+            else lo = mi + 1;
+        }
+        return lo;
+    }
+}
