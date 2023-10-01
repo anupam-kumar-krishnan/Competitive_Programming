d0218c1e3ea1964a10cb03c6775bb672286420b1
Sanvi <107338001+sanvi-05@users.noreply.github.com>
2023-10-01 20:35:44 +0530

    Update Factorial.java
    I have made changes of finding the factorial recursively.

--- All Languages/Java Codes/Factorial.java
+++ All Languages/Java Codes/Factorial.java
@@ -1,15 +1,21 @@
 import java.util.*;
 class Factorial{
+    public static long fact(int n) {
+        if (n == 0 || n == 1) {
+            // Base case: Factorial of 0 and 1 is 1
+            return 1;
+        } else {
+            // Recursive case: Factorial of n is n times factorial of (n-1)
+            return n * fact(n - 1);
+        }
+    }
     public static void main(String[] args){
         //Factorial of n is the product of all positive descending integers.
         System.out.println("Enter Integer:");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int f=1;
-        for(int i=1; i<=num; i++){
-            f = f*i;
-        }
-        System.out.println("Factirial of" + num + " is " + f);
+        long result = fact(num);
+        System.out.println("Factirial of " + num + " is " + result);
 
     }
 }
