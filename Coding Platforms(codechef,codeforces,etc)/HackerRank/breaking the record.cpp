049c3b9d2de491e9fed3f73ce80bc64b605a663e
avani07joshi <100960099+avani07joshi@users.noreply.github.com>
2023-10-18 19:21:24 +0530

    Create breaking the record.cpp

---
+++ Coding Platforms(codechef,codeforces,etc)/HackerRank/breaking the record.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+vector<int> breakingRecords(vector<int> scores) {
+    int max= scores[0];
+    int min= scores[0];
+    int max_count=0;
+    int min_count=0;
+    vector<int>ret;
+    
+    for(int i; i<scores.size(); i++){
+        if(scores[i]>max){
+            max=scores[i];
+            max_count++;
+        }
+        else if(scores[i]<min){
+            min=scores[i];
+            min_count++;
+        }
+        
+    }
+    ret.push_back(max_count);
+    ret.push_back(min_count);
+    return ret;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split(rtrim(scores_temp_temp));
+
+    vector<int> scores(n);
+
+    for (int i = 0; i < n; i++) {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    vector<int> result = breakingRecords(scores);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
